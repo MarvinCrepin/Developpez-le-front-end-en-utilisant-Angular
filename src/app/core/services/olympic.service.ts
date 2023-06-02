e1d26d17d1903e90672005cea3c065c13c09e541
MarvinCrepin <marvincrepin@gmail.com>
2023-06-02 14:13:06 +0200

    dev: olympic service has now error handling and country name filter

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -1,31 +1,40 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
-
+import { Router } from '@angular/router';
+import { BehaviorSubject, throwError } from 'rxjs';
+import { catchError, tap, map } from 'rxjs/operators';
+import { Olympic } from '../models/Olympic';
 @Injectable({
   providedIn: 'root',
 })
 export class OlympicService {
   private olympicUrl = './assets/mock/olympic.json';
   private olympics$ = new BehaviorSubject<any>(undefined);
+  constructor(private http: HttpClient, private router: Router) { }
 
-  constructor(private http: HttpClient) {}
-
-  loadInitialData() {
+  public loadInitialData() {
     return this.http.get<any>(this.olympicUrl).pipe(
       tap((value) => this.olympics$.next(value)),
       catchError((error, caught) => {
-        // TODO: improve error handling
-        console.error(error);
-        // can be useful to end loading state and let the user know something went wrong
         this.olympics$.next(null);
         return caught;
       })
     );
   }
 
-  getOlympics() {
+  public getOlympics() {
     return this.olympics$.asObservable();
   }
+
+  public getOlympicByCountryName = (country: string) =>
+    this.olympics$.pipe(
+      map(olympics => olympics.find((olympic: Olympic) =>
+        olympic.country.toLocaleLowerCase() === country.toLocaleLowerCase()
+      )));
+
+  errorWithOlympics$ = throwError(() => {
+    const error: any = new Error(`There is an error with fetching Olympics data.`);
+    error.timestamp = Date.now();
+    return error;
+  });
 }
